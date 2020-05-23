160389260a15e2f3c3fcaa46816c4d815d604d26
tterimaa <tterimaa@gmail.com>
2020-05-23 14:27:44 +0300

    Use async await in routes

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -21,56 +21,58 @@ router.get(
   }
 );
 
-router.post("/register-employee", employeeValidator, (req, res, _next) => {
-  userService
-    .addEmployee(req.body)
-    .then((user) => {
-      res.status(200).json({ success: true, msg: user });
-    })
-    .catch((err) => res.json({ success: false, msg: err }));
+router.post("/register-employee", employeeValidator, async (req, res, next) => {
+  try {
+    const user = await userService.addEmployee(req.body);
+    res.status(200).json({ success: true, msg: user });
+  } catch (e) {
+    return next(e);
+  }
 });
 
-router.post("/login-employee", loginValidator, (req, res, _next) => {
+router.post("/login-employee", loginValidator, async (req, res, next) => {
   const { email, password } = req.body;
-  userService
-    .findEmployeeByEmail(email)
-    .then((user) => {
-      if (userService.passwordIsValid(password, user)) {
-        const tokenObject = issueJWT(user);
-        res.status(200).json({
-          success: true,
-          token: tokenObject.token,
-          expires: tokenObject.expires,
-        });
-      } else res.status(401).json({ success: false, msg: "Wrong password" });
-    })
-    .catch((err) => res.json({ success: false, msg: err }));
+  try {
+    const user = await userService.findEmployeeByEmail(email);
+    if (userService.passwordIsValid(password, user)) {
+      const tokenObject = issueJWT(user);
+      res.status(200).json({
+        success: true,
+        token: tokenObject.token,
+        expires: tokenObject.expires,
+      });
+    } else res.status(401).json({ success: false, msg: "Wrong password" }); //TODO: throw custom error here
+  } catch (e) {
+    return next(e);
+  }
 });
 
-router.post("/register-company", companyValidator, (req, res, _next) => {
-  userService
-    .addCompany(req.body)
-    .then((user) => {
-      res.status(200).json({ success: true, msg: user });
-    })
-    .catch((err) => res.json({ success: false, msg: err }));
+router.post("/register-company", companyValidator, async (req, res, next) => {
+  try {
+    const user = await userService.addCompany(req.body);
+    res.status(200).json({ success: true, msg: user });
+  } catch (e) {
+    return next(e);
+  }
 });
 
-router.post("/login-company", loginValidator, (req, res, _next) => {
+// TODO: Unite logins into one route, only one line difference now
+// Should login route only handle login and give all the other logic to userService (or loginService)
+router.post("/login-company", loginValidator, async (req, res, next) => {
   const { email, password } = req.body;
-  userService
-    .findCompanyByEmail(email)
-    .then((user) => {
-      if (userService.passwordIsValid(password, user)) {
-        const tokenObject = issueJWT(user);
-        res.status(200).json({
-          success: true,
-          token: tokenObject.token,
-          expires: tokenObject.expires,
-        });
-      } else res.status(401).json({ success: false, msg: "Wrong password" });
-    })
-    .catch((err) => res.json({ success: false, msg: err.toString() }));
+  try {
+    const user = await userService.findCompanyByEmail(email);
+    if (userService.passwordIsValid(password, user)) {
+      const tokenObject = issueJWT(user);
+      res.status(200).json({
+        success: true,
+        token: tokenObject.token,
+        expires: tokenObject.expires,
+      });
+    } else res.status(401).json({ success: false, msg: "Wrong password" }); //TODO: throw custom error here
+  } catch (e) {
+    return next(e);
+  }
 });
 
 export default router;
