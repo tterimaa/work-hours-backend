c2c8275967ae21b88a4b44b9f679e3469a2d56b3
tterimaa <tterimaa@gmail.com>
2020-06-27 13:19:48 +0300

    set email as unique

--- src/models/User.ts
+++ src/models/User.ts
@@ -4,7 +4,7 @@ import { IUser } from "../types";
 const documentName = "User";
 
 const UserSchema = new Schema({
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   role: String,
   firstname: String,
   lastname: String,
